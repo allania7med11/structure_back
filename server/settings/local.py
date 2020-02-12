c42572bc22b74401124bded406e0926f9e5d170e
allania7med11 <allania7med11@gmail.com>
2020-02-12 15:07:46 +0100

    production

---
+++ server/settings/local.py
@@ -0,0 +1,15 @@
+
+from .base import *
+DEBUG = True
+ALLOWED_HOSTS = ["0.0.0.0","localhost",'127.0.0.1']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'structure_7_2_2020',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass', 
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+LOGIN_REDIRECT_URL = "http://localhost" + '/projects/'
---
+++ server/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+DEBUG = False
+ALLOWED_HOSTS = [
+    "0.0.0.0","localhost",'127.0.0.1',
+    'legacy.effectivewebapp.com',"165.22.119.28","www.legacy.effectivewebapp.com"]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'structurefull',
+        'USER': "ahmed",
+        'PASSWORD': "Chorbane.Kairouan.5300",
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+LOGIN_REDIRECT_URL = "legacy.effectivewebapp.com" + '/projects/'
