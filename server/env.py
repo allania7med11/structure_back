98c733dca7dcc580097abf0d82769c96502774d2
allania7med11 <allania7med11@gmail.com>
2020-02-22 18:27:57 +0100

    production

--- server/env.py
+++ server/env.py
@@ -1,13 +1,13 @@
-Docker=False
-if Docker==True:
+Production = False
+if Production==True:
     database={'NAME': 'postgres','USER': 'postgres','HOST': 'db','PORT': 5432}
     clientUrl="http://localhost"
     cors={"list":[],"app":[],"middleware":[],"session":'Lax'}
 else :
     database={'NAME': 'structure_7_2_2020','USER': 'myuser','PASSWORD': 'mypass', 'HOST': 'localhost','PORT': 5432}
-    clientUrl="http://localhost"
+    clientUrl="http://localhost:1000"
     cors={
-        "list":["http://localhost"],
+        "list":["http://localhost:1000"],
         "app":["corsheaders"],
         "middleware":['corsheaders.middleware.CorsMiddleware'],
         "session":None}
