20cc44b2e19af37b70bb5454a81d824c85d32929
allania7med11 <allania7med11@gmail.com>
2023-04-15 20:57:38 +0000

    Add test option to docker container

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ python manage.py migrate
 if [ "$ENVIRONMENT" = "dev" ]; then
     python manage.py collectstatic --noinput
     python manage.py runserver $PORT
+elif [ "$ENVIRONMENT" = "test" ]; then
+    python manage.py collectstatic --noinput
+    while :; do sleep 2073600; done
 else
     gunicorn server.wsgi:application --bind 0.0.0.0:$PORT
 fi
