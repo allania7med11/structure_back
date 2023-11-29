4b1fcde7c8958a134b243ac2cb13484e2dc76f0b
allania7med11 <allania7med11@gmail.com>
2023-11-29 22:20:37 +0000

    update structure run

--- run.sh
+++ run.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+
 echo "envs $ENVIRONMENT $PORT"
 python manage.py makemigrations
-python manage.py migrate 
+python manage.py migrate
+
+if [ "$COLLECTSTATIC" = "True" ]; then
+    python manage.py collectstatic --noinput
+fi
+
 if [ "$ENVIRONMENT" = "dev" ]; then
-    python manage.py collectstatic --noinput
-    python manage.py runserver $PORT
-elif [ "$ENVIRONMENT" = "test" ]; then
-    python manage.py collectstatic --noinput
-    while :; do sleep 2073600; done
+    python manage.py runserver 0.0.0.0:$PORT
+elif [ "$ENVIRONMENT" = "debug" ]; then
+    sleep infinity
 else
     gunicorn server.wsgi:application --bind 0.0.0.0:$PORT
 fi
